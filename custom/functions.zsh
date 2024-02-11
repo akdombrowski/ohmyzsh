087d25db6a911281e81416560ea439a61a85c7dd
Anthony Dombrowski <akdflyinvee73@gmail.com>
2024-02-11 11:23:22 -0600

    new fn's

--- custom/functions.zsh
+++ custom/functions.zsh
@@ -1,12 +1,96 @@
+copy() {
+  # $opt will hold the current option
+  local opt
+  while getopts ab: opt; do
+    # loop continues till options finished
+    # see which pattern $opt matches...
+    case $opt in
+    a)
+      print "Option a's value = $OPTARG"
+      ;;
+    b)
+      print "Option b's value = $OPTARG"
+      ;;
+      # matches a question mark
+      # (and nothing else, see text)
+    \?)
+      print Bad option, aborting.
+      return 1
+      ;;
+    esac
+  done
+  ((OPTIND > 1)) && shift "$((OPTIND - 1))"
+
+  echo -n "$1" | xclip -selection clipboard
+  echo "$1 ---> copied to clipboard"
+}
+
+rndSuffix() {
+  # $opt will hold the current option
+  local opt
+  # removed the ":" so that the value following the "-c" flag wouldn't be read
+  # with that flag
+  while getopts cx: opt; do
+    # loop continues till options finished
+    # see which pattern $opt matches...
+    case $opt in
+    c)
+      local wantsCopyToClipboard=1
+      ;;
+    x)
+      local extension=".$OPTARG"
+      ;;
+      # matches a question mark
+      # (and nothing else, see text)
+    \?)
+      print Bad option, aborting.
+      return 1
+      ;;
+    esac
+  done
+  ((OPTIND > 1)) && shift "$((OPTIND - 1))"
+  # print "Remaining arguments are: $*"
+
+  uuid="$(uuidgen -r)"
+  rnd="$1-${uuid}${extension}"
+
+  if [[ -n "$wantsCopyToClipboard" ]]; then
+    echo -n "$rnd" | xclip -selection clipboard
+    echo "$rnd"
+  fi
+}
+
+rndPrefix() {
+  uuid="$(uuidgen -r)"
+  arg0="$1"
+  rnd="${uuid}-${arg0}"
+  echo "$rnd"
+}
+
+rndUUID() {
+  local uuidr=$(uuidgen -r)
+  copy "$uuidr"
+}
+
+kaptchame() {
+  local uuidr=$(uuidgen -r)
+  local kaptchame="kaptcha-me-$uuidr"
+  copy "$kaptchame"
+}
+
+magickToolAliases() {
+  als=$(alias | grep "magick")
+  echo "$als"
+}
+
 # testpromptdir() {
 #   echo 'echo ${+PROMPT_PERCENT}'
 #   echo ${+PROMPT_PERCENT}
 #   echo ""
-  
+
 #   echo 'echo $3~'
 #   echo $3~
 #   echo ""
-
 
 #   echo 'echo PROMPT_PERCENT=1'
 #   PROMPT_PERCENT=1
@@ -16,11 +100,11 @@
 #   echo ${PROMPT_PERCENT}
 #   echo ""
 #   echo ""
-  
+
 #   echo 'echo $3~'
 #   echo $3~
 #   echo ""
-  
+
 #   echo 'echo %3~'
 #   echo %3~
 #   echo ""
@@ -50,7 +134,7 @@
 #   echo ""
 #   if [[ "$~" == "$HOME" ]]; then
 #     echo "\n\n ***** parent is dir ***** \n\n"
-#   else 
+#   else
 #     echo " parent \"$~\" is not a dir \n"
 #   fi
 #   echo ""
@@ -59,7 +143,7 @@
 #   echo ""
 #   if [[ $~ == "$HOME" ]]; then
 #     echo "\n\n ***** parent is dir ***** \n\n"
-#   else 
+#   else
 #     echo " parent '$~' is not a dir \n"
 #   fi
 #   echo ""
@@ -68,7 +152,7 @@
 #   echo ""
 #   if [[ -d $~ ]]; then
 #     echo "\n\n ***** parent is dir ***** \n\n"
-#   else 
+#   else
 #     echo " parent '$~' is not a dir \n"
 #   fi
 #   echo ""
@@ -77,7 +161,7 @@
 #   echo ""
 #   if [[ -d $3~ ]]; then
 #     echo "\n\n ***** parent is dir ***** \n\n"
-#   else 
+#   else
 #     echo " parent '$3~' is not a dir \n"
 #   fi
 #   echo ""
@@ -86,7 +170,7 @@
 #   echo ""
 #   if [[ -d "$3~" ]]; then
 #     echo "\n\n ***** parent is dir ***** \n\n"
-#   else 
+#   else
 #     echo " parent \"$3~\" is not a dir \n"
 #   fi
 #   echo ""
@@ -95,7 +179,7 @@
 #   echo ""
 #   if [[ -d '$3~' ]]; then
 #     echo "\n\n ***** parent is dir ***** \n\n"
-#   else 
+#   else
 #     echo " parent \'$3~\' is not a dir \n"
 #   fi
 #   echo ""
@@ -103,28 +187,27 @@
 #   set +x
 # }
 
-# testopts() {
-#   # $opt will hold the current option
-#   local opt
-#   while getopts ab: opt; do
-#     # loop continues till options finished
-#     # see which pattern $opt matches...
-#     case $opt in
-#     a)
-#       print Option a set
-#       ;;
-#     b)
-#       print Option b set to $OPTARG
-#       ;;
-#       # matches a question mark
-#       # (and nothing else, see text)
-#     \?)
-#       print Bad option, aborting.
-#       return 1
-#       ;;
-#     esac
-#   done
-#   ((OPTIND > 1)) && shift $((OPTIND - 1))
-#   print Remaining arguments are: $*
-# }
-
+testopts() {
+  # $opt will hold the current option
+  local opt
+  while getopts ab: opt; do
+    # loop continues till options finished
+    # see which pattern $opt matches...
+    case $opt in
+    a)
+      print Option a set
+      ;;
+    b)
+      print Option b set to $OPTARG
+      ;;
+      # matches a question mark
+      # (and nothing else, see text)
+    \?)
+      print Bad option, aborting.
+      return 1
+      ;;
+    esac
+  done
+  ((OPTIND > 1)) && shift "$((OPTIND - 1))"
+  print Remaining arguments are: $*
+}
