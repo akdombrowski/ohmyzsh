bf0688244d15320c52d9da959b48cc99ef1fca1e
Anthony Dombrowski <akdflyinvee73@gmail.com>
2024-04-29 00:30:08 -0500

    ls aliases

--- custom/my_aliases.zsh
+++ custom/my_aliases.zsh
@@ -21,5 +21,20 @@ alias ncu="yarn dlx npm-check-updates"
 alias npx_ncu="npx npm-check-updates"
 
 alias eless="ls -A -sS -p -h -gG"
+alias ls='ls -A --group-directories-first --tabsize=128'
+alias lscomma='ls -Am --group-directories-first --tabsize=128'
+alias l='ls -gh --group-directories-first --tabsize=128'
+alias la='ls -gAh --group-directories-first --tabsize=128'
+alias ldot='ls --group-directories-first -gd .*  --tabsize=128'
+alias lsa='ls --group-directories-first -gah --tabsize=128'
+alias lsrecurs='ls --group-directories-first -gARh --tabsize=128'
+alias lt='ls --group-directories-first -gth --tabsize=128'
+alias ll='ls --group-directories-first -l --tabsize=128'
+alias ll='ll --group-directories-first -lh --tabsize=128'
+alias lla='ls --group-directories-first -lAh --tabsize=128'
+alias lldot='ls --group-directories-first -ld .* --tabsize=128'
+alias llsa='ls --group-directories-first -lah --tabsize=128'
+alias llsrecurs='ls --group-directories-first -lARh --tabsize=128'
+alias llt='ls --group-directories-first -lth --tabsize=128'
 
 alias subl='/mnt/a/Program\ Files/Sublime\ Text/subl.exe'
