d588f124f9847a97dff9c61025483826115d7d31
Anthony Dombrowski <akdflyinvee73@gmail.com>
2024-07-08 14:04:10 -0500

    new file in custom folder to add customizations to .zshrc instead of adding directly to that file

---
+++ custom/add_to_zshrc.zsh
@@ -0,0 +1,6 @@
+# Customizations to .zshrc file
+
+# rather than adding stuff to the .zshrc file in the home dir which isnt being syncd with git
+# this file is read and can add things like path variables
+
+export PATH="$PATH:/usr/local/cuda-12.5/bin"
