5ecc7d38b3f416ae16801f85d358e6995a19168a
Anthony Dombrowski <akdflyinvee73@gmail.com>
2024-11-15 16:38:11 -0600

    tweak to aliases

--- custom/aliases/mint_aliases.zsh
+++ custom/aliases/mint_aliases.zsh
@@ -9,12 +9,19 @@ alias whichAlias="echo mint_aliases"
 alias whichFileManager="echo $(xdg-mime query default inode/directory)"
 alias setDefaultFileMgr="echo $(xdg-mime default org.kde.dolphin.desktop inode/directory)"
 
-
 alias subl="/opt/sublime_text/sublime_text --launch-or-new-window "
+# sublime_text [arguments] -               Edit stdin
+alias sublStdIn="/opt/sublime_text/sublime_text -n - "
+# sublime_text [arguments] - >out          Edit stdin and write the edit to stdout
+alias sublStdIn2Out="/opt/sublime_text/sublime_text -n - >out "
 
 alias inputDevicesList="xinput --list --short"
 alias ergo="xinput --list-props \"ERGO M575 Mouse\""
-alias setErgoFeed="xinput set-ptr-feedback \"ERGO M575 Mouse\" 0 3 1"
-alias setErgoSpeed="xinput set-prop \"ERGO M575 Mouse\" \"libinput Accel Speed\" 1"
-
+alias ergoFeed="xinput set-ptr-feedback \"ERGO M575 Mouse\" 0 3 1"
+alias ergoSpeed="xinput set-prop \"ERGO M575 Mouse\" \"libinput Accel Speed\" 1"
+# needs 3 inputs 'threshold speed denominator' see 'ergoFeed' above
+alias setErgoFeed="xinput set-ptr-feedback \"ERGO M575 Mouse\" "
+# needs 1 input for accel speed ' see 'ergoSpeed' above
+alias setErgoSpeed="xinput set-prop \"ERGO M575 Mouse\" \"libinput Accel Speed\" "
+# script which sets both ergo accel speed and feedback
 alias setErgoFast=". $SET_FAST_MOUSE_SCRIPT 1"
---
+++ custom/scripts/setFastMouse.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# shellcheck source=../funs/mouseSpeed.zsh
+. "$ZSH_CUSTOM/funs/mouseSpeed.zsh"
+
+# printf "\nCURRENT ergo speed: %s \n" "$(get_ergo_speed)"
+
+# printf "\$1 = %s\n" "$1"
+
+# printf "\nsetting accel speed...\n\n"
+
+set_ergo_mouse_speed -s "$1"
+
+# printf "\ndone\n\nsetting feedback...\n\n"
+
+set_ergo_feedback
+
+# printf "\n\ndone\n"
+
+# printf "NEW ergo settings: \n\n"
+printf "\t\tSPEED: \n%s \n\n---\n\n" "$(get_ergo_speed)"
+printf "\t\tFEEDBACK: \n%s \n" "$(get_ergo_feedbacks)"
