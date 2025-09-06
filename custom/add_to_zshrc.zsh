41418ce9dd422f3f565ac7690db0863a125ba4cb
akdombrowski <akdflyinvee73@gmail.com>
2025-09-06 11:38:00 -0500

    removed debug log

--- custom/add_to_zshrc.zsh
+++ custom/add_to_zshrc.zsh
@@ -34,8 +34,6 @@ fi
 # autostart ssh
 source "$SCRIPTZ/init-ssh.zsh"
 
-echo "started ssh"
-
 # Set name of the theme to load --- if set to "random", it will
 # load a random theme each time oh-my-zsh is loaded, in which case,
 # to know which specific one was loaded, run: echo $RANDOM_THEME
