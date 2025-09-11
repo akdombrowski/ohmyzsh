1d1add8eb8074a6251c6b9a62e81037e03c734f2
akdombrowski <akdflyinvee73@gmail.com>
2025-09-11 08:05:35 -0500

    ollama env vars

--- custom/env_vars/common_vars.zsh
+++ custom/env_vars/common_vars.zsh
@@ -28,3 +28,35 @@ export MACOS_ALIASES="macos_aliases"
 
 # ip of desktop
 export LENNYDESK='15'
+
+# Ollama
+# Show additional debug information (e.g. OLLAMA_DEBUG=1) 
+OLLAMA_DEBUG=''
+# IP Address for the ollama server (default 127.0.0.1:11434) 
+OLLAMA_HOST=''
+# The duration that models stay loaded in memory (default "5m") 
+OLLAMA_KEEP_ALIVE=''
+# Maximum number of loaded models per GPU 
+OLLAMA_MAX_LOADED_MODELS=''
+# Maximum number of queued requests 
+OLLAMA_MAX_QUEUE=''
+# The path to the models directory 
+OLLAMA_MODELS=''
+# Maximum number of parallel requests 
+OLLAMA_NUM_PARALLEL=''
+# Do not prune model blobs on startup 
+OLLAMA_NOPRUNE=''
+# A comma separated list of allowed origins 
+OLLAMA_ORIGINS='localhost,127.0.0.1'
+# Always schedule model across all GPUs 
+OLLAMA_SCHED_SPREAD='true'
+# Enabled flash attention 
+OLLAMA_FLASH_ATTENTION=''
+# Quantization type for the K/V cache (default: f16) 
+OLLAMA_KV_CACHE_TYPE='f32'
+# Set LLM library to bypass autodetection 
+OLLAMA_LLM_LIBRARY=''
+# Reserve a portion of VRAM per GPU (bytes) 
+OLLAMA_GPU_OVERHEAD='1024'
+# How long to allow model loads to stall before giving up (default "5m") 
+OLLAMA_LOAD_TIMEOUT='1m'
