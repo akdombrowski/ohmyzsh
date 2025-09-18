4c1864f3ffe8ebc65671aa7968d2e2853144b54a
akdombrowski <akdflyinvee73@gmail.com>
2025-09-18 17:13:57 -0500

    uv and uvx autocompletion added

--- custom/os-specific-config/win_wsl.zsh
+++ custom/os-specific-config/win_wsl.zsh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
+
 source $ALIASEZ/wsl_aliases.zsh
 source "$ENV_VARS_DIR/wsl_vars.zsh"
+
+# enable shell autocompletion for uv and uvx commands
+echo 'eval "$(uv generate-shell-completion zsh)"' >> ~/.zshrc
+echo 'eval "$(uvx --generate-shell-completion zsh)"' >> ~/.zshrc
