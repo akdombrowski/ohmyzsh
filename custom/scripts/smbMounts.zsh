f7a63524c60509d0ca729d7567ae435291941894
akdombrowski <akdflyinvee73@gmail.com>
2025-11-29 22:19:19 -0600

    ups

--- custom/scripts/smbMounts.zsh
+++ custom/scripts/smbMounts.zsh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+LENNYDESK_IP="//192.168.86.$LENNYDESK"
+LENNYDESK_AHHEM="$LENNYDESK_IP/ahhem"
+LENNYDESK_AHHEME="$LENNYDESK_IP/ahheme"
+LENNYDESK_AHHEMF="$LENNYDESK_IP/ahhemf"
+HOME_LENNYDESK="$HOME/.lennydesk"
+
 export AHHEM="/mnt/ahhem"
 export E="ahheme"
 export D="ahhemd"
@@ -15,31 +21,68 @@ count() {
   echo $numberOfMp4s
 }
 
-
 # list all available smb shares from host
 # echo "# list all available smb shares from host"
 # echo "smbclient -L 192.168.86.15 -U \"username%password\""
-sudo mount --onlyonce -m -t cifs "//192.168.86.$LENNYDESK/ahhem" "${AHHEM}/${D}" \
-  -o $(cat "$HOME/.lennydesk")
-sudo mount --onlyonce -m -t cifs "//192.168.86.$LENNYDESK/ahheme" "${AHHEM}/${E}" \
-  -o $(cat "$HOME/.lennydesk")
-sudo mount --onlyonce -m -t cifs "//192.168.86.$LENNYDESK/ahhemf" "${AHHEM}/${F}" \
-  -o $(cat "$HOME/.lennydesk")
 
-# Auto-mount LennyDesk share if not already mounted
-if ! mountpoint -q "${AHHEM}/${D}"; then
-    sudo mount --onlyonce -m -t cifs "//192.168.86.$LENNYDESK/ahhem" "${AHHEM}/${D}" \
-      -o $(cat "$HOME/.lennydesk")
-fi
+mount() {
+  sudo mount --onlyonce -m -t cifs "//192.168.86.$LENNYDESK/ahhem" "${AHHEM}/${D}" \
+    -o $(cat "$HOME_LENNYDESK")
+  sudo mount --onlyonce -m -t cifs "//192.168.86.$LENNYDESK/ahheme" "${AHHEM}/${E}" \
+    -o $(cat "$HOME_LENNYDESK")
+  sudo mount --onlyonce -m -t cifs "//192.168.86.$LENNYDESK/ahhemf" "${AHHEM}/${F}" \
+    -o $(cat "$HOME_LENNYDESK")
+}
 
-# Auto-mount LennyDesk share if not already mounted
-if ! mountpoint -q "${AHHEM}/${E}"; then
-    sudo mount -m -t cifs "//192.168.86.$LENNYDESK/ahheme" "${AHHEM}/${E}" \
-      -o $(cat "$HOME/.lennydesk")
-fi
+a_hhem_uto_mnt() {
+  # Auto-mount LennyDesk share if not already mounted
+  arg1="$1"
+  arg2="$2"
+  if ! mountpoint -q "${arg1}"; then
+    sudo mount --onlyonce -m -t cifs "$LENNYDESK_AHHEM" "$arg1" \
+      -o $(cat "$HOME_LENNYDESK")
+  fi
+}
 
-# Auto-mount LennyDesk share if not already mounted
-if ! mountpoint -q "${AHHEM}/${F}"; then
-    sudo mount -m -t cifs "//192.168.86.$LENNYDESK/ahhemf" "${AHHEM}/${F}" \
-      -o $(cat "$HOME/.lennydesk")
-fi
+auto_mnt() {
+  # Auto-mount LennyDesk share if not already mounted
+  mnt_path="$1"
+  smb_path="$2"
+  if ! mountpoint -q "${arg1}"; then
+    sudo mount --onlyonce -m -t cifs "$smb_path" "$mnt_path" \
+      -o $(cat "$HOME_LENNYDESK")
+  fi
+}
+
+# # Auto-mount LennyDesk share if not already mounted
+# if ! mountpoint -q "${AHHEM}/${D}"; then
+#     sudo mount --onlyonce -m -t cifs "//192.168.86.$LENNYDESK/ahhem" "${AHHEM}/${D}" \
+#       -o $(cat "$HOME_LENNYDESK")
+# fi
+
+# # Auto-mount LennyDesk share if not already mounted
+# if ! mountpoint -q "${AHHEM}/${E}"; then
+#     sudo mount -m -t cifs "//192.168.86.$LENNYDESK/ahheme" "${AHHEM}/${E}" \
+#       -o $(cat "$HOME_LENNYDESK")
+# fi
+
+# # Auto-mount LennyDesk share if not already mounted
+# if ! mountpoint -q "${AHHEM}/${F}"; then
+#     sudo mount -m -t cifs "//192.168.86.$LENNYDESK/ahhemf" "${AHHEM}/${F}" \
+#       -o $(cat "$HOME_LENNYDESK")
+# fi
+
+auto_mnt "${AHHEM}/${D}" "$LENNYDESK_AHHEM"
+auto_mnt "${AHHEM}/${E}" "$LENNYDESK_AHHEME"
+auto_mnt "${AHHEM}/${F}" "$LENNYDESK_AHHEMF"
+auto_mnt "/mnt/H" "$LENNYDESK_IP/SeagateH"
+auto_mnt "/mnt/W" "$LENNYDESK_IP/WD Elements"
+
+echo ""
+echo "mounts:"
+echo "${AHHEM}/${D} > $LENNYDESK_AHHEM"
+echo "${AHHEM}/${E} > $LENNYDESK_AHHEME"
+echo "${AHHEM}/${F} > $LENNYDESK_AHHEMF"
+echo "/mnt/H > $LENNYDESK_IP/SeagateH"
+echo "/mnt/H > $LENNYDESK_IP/WD Elements"
+echo ""
