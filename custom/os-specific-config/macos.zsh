940106ba33924d7fad8e0b143c3603325a0b1895
Anthony Dombrowski <akdflyinvee73@gmail.com>
2026-03-08 09:56:27 -0500

    use home env var

--- custom/os-specific-config/macos.zsh
+++ custom/os-specific-config/macos.zsh
@@ -48,16 +48,18 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
 # perl
-PATH="${PATH:+${PATH}}:/home/akdombrowski/perl5/bin"
+PATH="${PATH:+${PATH}}:/$HOME/perl5/bin"
 export PATH
-PERL5LIB="/home/akdombrowski/perl5/lib/perl5${PERL5LIB:+:${PERL5LIB}}"
+PERL5LIB="/$HOME/perl5/lib/perl5${PERL5LIB:+:${PERL5LIB}}"
 export PERL5LIB
-PERL_LOCAL_LIB_ROOT="/home/akdombrowski/perl5${PERL_LOCAL_LIB_ROOT:+:${PERL_LOCAL_LIB_ROOT}}"
+PERL_LOCAL_LIB_ROOT="/$HOME/perl5${PERL_LOCAL_LIB_ROOT:+:${PERL_LOCAL_LIB_ROOT}}"
 export PERL_LOCAL_LIB_ROOT
-PERL_MB_OPT="--install_base \"/home/akdombrowski/perl5\""
+PERL_MB_OPT="--install_base \"/$HOME/perl5\""
 export PERL_MB_OPT
-PERL_MM_OPT="INSTALL_BASE=/home/akdombrowski/perl5"
+PERL_MM_OPT="INSTALL_BASE=/$HOME/perl5"
 export PERL_MM_OPT
+
+export PATH="${PATH}:$HOME/Library/Python/3.9/bin"
 
 # # set fast mouse speed
 # setErgoFast
