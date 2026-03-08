774b4a970bbfb74957e03f9c017aec85f5ee92a2
Anthony Dombrowski <akdflyinvee73@gmail.com>
2026-03-08 08:04:32 -0500

    fixed nvm loading

--- custom/os-specific-config/macos.zsh
+++ custom/os-specific-config/macos.zsh
@@ -44,7 +44,7 @@ source "$ENV_VARS_DIR/macos_vars.zsh"
 
 # nvm
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.zsh" # This loads nvm
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
 # perl
