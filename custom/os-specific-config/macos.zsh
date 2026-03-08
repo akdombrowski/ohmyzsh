b34e4f508ffa9ce02f6146d3d0adfbac68f563ff
Anthony Dombrowski <akdflyinvee73@gmail.com>
2026-03-08 09:58:19 -0500

    perl install

--- custom/os-specific-config/macos.zsh
+++ custom/os-specific-config/macos.zsh
@@ -56,8 +56,11 @@ PERL_LOCAL_LIB_ROOT="/$HOME/perl5${PERL_LOCAL_LIB_ROOT:+:${PERL_LOCAL_LIB_ROOT}}
 export PERL_LOCAL_LIB_ROOT
 PERL_MB_OPT="--install_base \"/$HOME/perl5\""
 export PERL_MB_OPT
-PERL_MM_OPT="INSTALL_BASE=/$HOME/perl5"
+
+# PERL_MM_OPT="INSTALL_BASE=/$HOME/perl5"
+PERL_MM_OPT="INSTALL_BASE=$HOME/perl5" cpan local::lib
 export PERL_MM_OPT
+eval "$(perl -I$HOME/perl5/lib/perl5 -Mlocal::lib=$HOME/perl5)"
 
 export PATH="${PATH}:$HOME/Library/Python/3.9/bin"
 
