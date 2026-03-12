b0f5b16fa9e1e00591d6b1eb071986aa7b2b95c6
Anthony Dombrowski <akdflyinvee73@gmail.com>
2026-03-12 13:52:08 -0500

    stop cpan logging and init on terminal open

--- custom/os-specific-config/macos.zsh
+++ custom/os-specific-config/macos.zsh
@@ -58,9 +58,10 @@ PERL_MB_OPT="--install_base \"/$HOME/perl5\""
 export PERL_MB_OPT
 
 # PERL_MM_OPT="INSTALL_BASE=/$HOME/perl5"
-PERL_MM_OPT="INSTALL_BASE=$HOME/perl5" cpan local::lib
+# PERL_MM_OPT="INSTALL_BASE=$HOME/perl5" cpan local::lib
+PERL_MM_OPT="INSTALL_BASE=$HOME/perl5"
 export PERL_MM_OPT
-eval "$(perl -I$HOME/perl5/lib/perl5 -Mlocal::lib=$HOME/perl5)"
+# eval "$(perl -I$HOME/perl5/lib/perl5 -Mlocal::lib=$HOME/perl5)"
 
 export PATH="${PATH}:$HOME/Library/Python/3.9/bin"
 
